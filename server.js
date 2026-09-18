60c7f33650356078a819c8035ea646c8030a7f7d
captainomercder-129 <captainomercder@gmail.com>
2026-09-18 17:59:26 +0000

    Service role key kullan

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 app.get('/health', async (req, res) => {
   try {
